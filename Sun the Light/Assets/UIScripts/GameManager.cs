6f85e9e0aa95b4b122b44ccd703a4480d97b02c5
YujinJeon888888 <117812553+YujinJeon888888@users.noreply.github.com>
2022-11-12 23:01:02 +0900

    UI

--- Sun the Light/Assets/UIScripts/GameManager.cs
+++ Sun the Light/Assets/UIScripts/GameManager.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+    //씬 관리
+    public int stageIndex=0; 
+    public GameObject[] Stages;
+
     //리스타트버튼 가져오기
     public GameObject RestartButton;
     //플레이어가 뭐 하면 포인트올라가게(플레이어에서 포인트 불러와서 올리기)
@@ -63,14 +66,16 @@ public class GameManager : MonoBehaviour
 
         // 재시작하게 되면 timeScale = 1로 시간을 복구
         Time.timeScale = 1;
-        SceneManager.LoadScene(0);//처음 씬으로 돌아간다.
+        SceneManager.LoadScene(0);//처음 씬으로 돌아간다.(씬 다 정해지면 인덱스 게임화면 나오게 바꾸기)
         score = 0;//재시작버튼 누르면 score 0으로 초기화
         
 
         //여기가 문제
         //리스타트하면 게임오버메세지도 사라진다
-        //gameOverMsg.gameObject.SetActive(false);
+        gameOverMsg.gameObject.SetActive(false);
       
     }
+
+
 
 }
